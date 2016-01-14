0bbaa1591ad2b1786fb91c169e9c81bc618fc7b2
Manuel Jesús de la Calle Brihuega <manuel.brihuega@gmail.com>
2016-01-14 11:00:33 +0100

    Cambios iniciales

--- setup.py
+++ setup.py
@@ -17,28 +17,28 @@ setup(
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
     install_requires=[
-        'Django==1.6.1'
-        'MySQL-python==1.2.5'
-        'Pillow==2.3.0'
-        'South==0.8.4'
-        'Unidecode==0.04.16'
-        'amqp==1.4.2'
-        'anyjson==0.3.3'
-        'billiard==3.3.0.14'
-        'celery==3.1.8'
-        'django-celery==3.1.1'
-        'django-cors-headers==0.12'
-        'django-extensions==1.3.3'
-        'django-tasks==0.3'
-        'docopt==0.4.0'
-        'kombu==3.0.10'
-        'mailchimp==2.0.7'
-        'pexpect==3.2'
-        'pytg==0.1'
-        'pytz==2013.9'
-        'requests==2.2.1'
-        'six==1.6.1'
-        'wsgiref==0.1.2'
+        'Django==1.6.1',
+        'MySQL-python==1.2.5',
+        'Pillow==2.3.0',
+        'South==0.8.4',
+        'Unidecode==0.04.16',
+        'amqp==1.4.2',
+        'anyjson==0.3.3',
+        'billiard==3.3.0.14',
+        'celery==3.1.8',
+        'django-celery==3.1.1',
+        'django-cors-headers==0.12',
+        'django-extensions==1.3.3',
+        'django-tasks==0.3',
+        'docopt==0.4.0',
+        'kombu==3.0.10',
+        'mailchimp==2.0.7',
+        'pexpect==3.2',
+        'pytg==0.1',
+        'pytz==2013.9',
+        'requests==2.2.1',
+        'six==1.6.1',
+        'wsgiref==0.1.2',
         'xlrd==0.9.3'
     ],
     dependency_links=[
