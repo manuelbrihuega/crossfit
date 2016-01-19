6adb6084f49cfbf4cd03745b7c4b0f5b525448e5
Manuel Jesús de la Calle Brihuega <manuel.brihuega@gmail.com>
2016-01-19 17:59:08 +0100

    Comenzando con la config para envio emails

--- wsgi/reservas/reservas/settings.py
+++ wsgi/reservas/reservas/settings.py
@@ -95,3 +95,4 @@ EMAIL_PORT=587
 EMAIL_HOST_USER="crossfitjerezdelafrontera@gmail.com"
 EMAIL_HOST_PASSWORD="Larios501"
 EMAIL_USE_TLS=True
+EMAIL_BACKEND = django.core.mail.backends.smtp.EmailBackend
