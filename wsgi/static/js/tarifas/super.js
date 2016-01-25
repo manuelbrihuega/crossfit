ad435997f4293311c7535468a4e2df1f8795e519
Manuel Jesús de la Calle Brihuega <manuel.brihuega@gmail.com>
2016-01-25 14:42:56 +0100

    Añadiendo pantalla tarifas

--- wsgi/static/js/tarifas/super.js
+++ wsgi/static/js/tarifas/super.js
@@ -5,8 +5,6 @@ function get_content() {
 		$.getScript(media_url+'js/aux/date.js'),
         $.getScript(media_url+'js/aux/modals.js'),
         $.getScript(media_url+'js/lib/sha1.js'),
-        $.getScript(media_url+'js/lib/jquery-latest.js'),
-        $.getScript(media_url+'js/lib/jquery.tablesorter.min.js'),
         $.ready.promise()
     ).then(function(){
 		$.post('partials/tarifas_super', function(template, textStatus, xhr) {
