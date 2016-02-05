2dc0efe17d06768e3b5e62f7f9dff39017bfaf59
Manuel Jesús de la Calle Brihuega <manuel.brihuega@gmail.com>
2016-02-05 14:50:38 +0100

    Añadiendo comunicaciones cliente

--- wsgi/reservas/reservas/aux/tickets.py
+++ wsgi/reservas/reservas/aux/tickets.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from datetime import datetime
 from reservas.models import Messages
 from reservas.aux.date import *
@@ -23,12 +24,12 @@ def add_message(ticket,text,way,offset):
             else:
                 user = 'No identificado'
             #send_email_ticket_message_supporter('info@taxible.com',ticket.title,message.text,user)
-        '''else:
-            if ticket.auth:
-                #send_email_ticket_message(ticket.auth.email,ticket.title,message.text)
-            else:
-                #send_email_ticket_message(ticket.email,ticket.title,message.text)
-        '''
+            '''else:
+                if ticket.auth:
+                    #send_email_ticket_message(ticket.auth.email,ticket.title,message.text)
+                else:
+                    #send_email_ticket_message(ticket.email,ticket.title,message.text)
+            '''
 
         return {'status':'success','response':'message_added', 'data':{ 'message':{ 'id':message.id, 'text':message.text, 'original_way':message.original_way, 'date':get_string_from_date(localdate) }    }}
     except Exception as e:
