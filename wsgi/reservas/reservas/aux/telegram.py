2570255c15de3ce1bb65b1e2e3b429b858a081dc
Manuel Jesús de la Calle Brihuega <manuel.brihuega@gmail.com>
2016-02-16 11:45:16 +0100

    Solucionando telegram

--- wsgi/reservas/reservas/aux/telegram.py
+++ wsgi/reservas/reservas/aux/telegram.py
@@ -8,7 +8,7 @@ def send_telegram(name,nick,phone,msg):
     cmd = patheo+'telegram-cli -k '+patheo+'../tg-server.pub'
     telegram = pexpect.spawn(cmd)
     # telegram.expect('0m',timeout=5)
-    index = telegram.expect(['0m', 'unread', pexpect.EOF, pexpect.TIMEOUT],timeout=5)
+    index = telegram.expect(['', 'unread', pexpect.EOF, pexpect.TIMEOUT],timeout=5)
     if index == 0 or index == 1:
         add_contact_method(telegram,name,nick,phone,msg)
     elif index == 2:
@@ -22,7 +22,7 @@ def send_telegram(name,nick,phone,msg):
 def add_contact_method(telegram,name,nick,phone,msg):
     add_contact_line = 'add_contact '+phone+' '+name
     telegram.sendline(add_contact_line)
-    index = telegram.expect(['0m', 'successfully', pexpect.EOF, pexpect.TIMEOUT],timeout=5)
+    index = telegram.expect(['', 'successfully', pexpect.EOF, pexpect.TIMEOUT],timeout=5)
     if index == 0 or index == 1:
         print telegram.after
         send_msg_method(telegram,nick,msg)
@@ -37,7 +37,7 @@ def add_contact_method(telegram,name,nick,phone,msg):
 def send_msg_method(telegram, nick, msg):
     send_msg_line = 'msg '+nick+' '+msg
     telegram.sendline(send_msg_line)   
-    index = telegram.expect(['0m', 'print_message', 'Bad', pexpect.EOF, pexpect.TIMEOUT],timeout=5)
+    index = telegram.expect(['', 'print_message', 'Bad', pexpect.EOF, pexpect.TIMEOUT],timeout=5)
     if index == 0 or index == 1:
         print 'SENT'
     elif index == 2:
